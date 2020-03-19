beb3df895ddc0621d84b9820d5b7a80225f8ba3e
Tom Lovelady <tom@true9.co.uk>
2020-03-19 14:13:23 +0000

    Fix production api url

--- src/environments/api.prod.ts
+++ src/environments/api.prod.ts
@@ -2,7 +2,7 @@ import { url as generateUrl } from './api-url';
 
 export const api = {
   protocol: 'https',
-  baseUrl: 'kind.co.uk',
+  baseUrl: 'api.kind.t9staging.co.uk',
   port: null,
   url: (endpoint: string, args: any = null) => {
     return generateUrl(api, endpoint, args);
