0808086f2101564b13c02cf3bbeec4bdd6fdf413
Tom Lovelady <tom@true9.co.uk>
2020-03-20 15:52:49 +0000

    Add google analytics code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+import { environment as e } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // @ts-ignore
+    gtag('config', e.analytics.trackingId);
+  }
+
+  ngAfterViewInit(): void {
+    this.router.events.subscribe(
+      event => {
+        if (event instanceof NavigationEnd) {
+          // @ts-ignore
+          window.ga('set', 'page', event.urlAfterRedirects);
+          // @ts-ignore
+          window.ga('send', 'pageview');
+        }
+      }
+    );
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,10 @@ import { api } from './api';
 
 export const environment = {
   production: false,
-  api
+  api,
+  analytics: {
+    trackingId: 'UA-161463013-1'
+  }
 };
 
 /*
@@ -12,4 +15,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
